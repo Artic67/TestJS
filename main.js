161d993b91b0c326ec3a7d4418da283494244a0f
Tema <sirartic67@gmail.com>
2021-04-30 02:16:34 -0700

    New Parser added

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 class Parser {
-    per() {
-        return 3;
+    getNameById(id, data) {
+        for (const el in data) {
+            let spl = data[el].split(";");
+            spl.pop();
+            console.log(spl);
+            if (spl[3] == id) return spl[0];
+        }
+        return undefined
     }
+
 }
 
 let a = Parser.prototype;
-console.log(a.per());
+console.log(a.getNameById("19126", ["Artem;18;human;91286;",
+"Andrey;16;human;19126;",
+"Mark;23;human;03632;",
+"Lily;21;human;52372;"]));
